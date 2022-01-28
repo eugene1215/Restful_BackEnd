ff30160bd5100d033dad90a69f0c33501e8a658d
eugene1215 <eugeneho1218@gmail.com>
2022-01-28 15:00:33 +0800

    fix 404 issue

--- src/main/java/com/example/microservice/controller/RestaurantController.java
+++ src/main/java/com/example/microservice/controller/RestaurantController.java
@@ -4,27 +4,41 @@ import java.util.ArrayList;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.stereotype.Controller;
 
 import com.example.microservice.model.Restaurant;
 import com.example.microservice.services.RestaurantService;
 
+@ComponentScan
 @RestController
 public class RestaurantController {
-	
+
 	@Autowired
 	private RestaurantService restaurantService;
-	
+
 	@GetMapping("/hello")
+//	@ResponseBody
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
 	}
 
 	@GetMapping("/")
+//	@ResponseBody
 	public ArrayList<Restaurant> index() {
 
 		return restaurantService.retrieveAllRestaurant();
+	}
+
+	@GetMapping("/abc")
+	@ResponseBody
+	public String abc() {
+
+		return "abc";
 	}
 
 }
--- src/main/java/com/example/microservice/microserviceApplication.java
+++ src/main/java/com/example/microservice/microserviceApplication.java
@@ -1,4 +1,4 @@
-package com.example.microservice.services;
+package com.example.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
