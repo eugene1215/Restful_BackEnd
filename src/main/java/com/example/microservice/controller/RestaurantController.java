bb1acfcb007843179372fafa4ab12be3644e64bd
eugene1215 <eugeneho1218@gmail.com>
2022-01-28 17:22:12 +0800

    add post feature

--- src/main/java/com/example/microservice/controller/RestaurantController.java
+++ src/main/java/com/example/microservice/controller/RestaurantController.java
@@ -1,15 +1,22 @@
 package com.example.microservice.controller;
 
+import java.net.URI;
 import java.util.ArrayList;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Loggers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.microservice.model.Restaurant;
 import com.example.microservice.services.RestaurantService;
@@ -17,6 +24,8 @@ import com.example.microservice.services.RestaurantService;
 @ComponentScan
 @RestController
 public class RestaurantController {
+
+//	private final Logger logger = L);
 
 	@Autowired
 	private RestaurantService restaurantService;
@@ -34,11 +43,22 @@ public class RestaurantController {
 		return restaurantService.retrieveAllRestaurant();
 	}
 
-	@GetMapping("/abc")
-	@ResponseBody
-	public String abc() {
 
-		return "abc";
+
+	@PostMapping("/restaurant/{id}")
+//	@ResponseBody
+	public ResponseEntity<Void> registerRastaurant(@PathVariable int id, @RequestBody Restaurant newRestaurant) {
+		HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");    
+		Restaurant restaurant = restaurantService.createRestaurant(id, newRestaurant);
+
+		if (restaurant == null) {
+			return ResponseEntity.noContent().build();
+		}
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(restaurant.getId())
+				.toUri();
+
+		return ResponseEntity.created(location).build();
 	}
 
 }
--- src/main/java/com/example/microservice/services/RestaurantService.java
+++ src/main/java/com/example/microservice/services/RestaurantService.java
@@ -1,5 +1,7 @@
 package com.example.microservice.services;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.ArrayList;
 
 import org.springframework.stereotype.Component;
@@ -9,13 +11,33 @@ import com.example.microservice.model.Restaurant;
 @Component
 public class RestaurantService {
 
-	private static ArrayList<Restaurant> restaurants = new ArrayList<>();
-	static {
-		Restaurant rest = new Restaurant("", 01, "", 5);
-		restaurants.add(rest);
-	}
+	private SecureRandom random = new SecureRandom();
+	
 
+
+	public Restaurant createRestaurant(int id, Restaurant restaurant) {
+//		Restaurant restaurant = new Restaurant("", 0, "", 0);
+//		restaurant.setName(name);
+//		restaurant.setId(id);
+//		restaurant.setDescription(description);
+//		restaurant.setRate(rate);
+		int randomId = new BigInteger(130, random).intValue();
+		restaurant.setId(randomId);
+
+		
+		return restaurant;
+	}
 	public ArrayList<Restaurant> retrieveAllRestaurant() {
+		ArrayList<Restaurant> restaurants = new ArrayList<>();
+//		restaurants.add(null)
 		return restaurants;
 	}
+	
+//	private static ArrayList<Restaurant> restaurants = new ArrayList<>();
+////static {
+//	Restaurant rest = new Restaurant("", 01, "", 5);
+//	restaurants.add(rest);
+////}
+	
+	
 }
