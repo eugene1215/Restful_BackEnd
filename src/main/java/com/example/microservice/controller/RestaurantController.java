b9f8f945aa404dc2cd2ebadabf0ed3c71f1ea2fc
eugene1215 <eugeneho1218@gmail.com>
2022-02-04 14:13:19 +0800

    1. add hot load depen.
    2. add insert rest for cloud db

--- src/main/java/com/example/microservice/controller/RestaurantController.java
+++ src/main/java/com/example/microservice/controller/RestaurantController.java
@@ -2,6 +2,7 @@ package com.example.microservice.controller;
 
 import com.example.microservice.model.RestaurantInfo;
 import com.example.microservice.services.RestaurantService;
+import com.google.gson.Gson;
 
 import java.net.URI;
 
@@ -20,9 +21,11 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
-
 
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -31,76 +34,75 @@ import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoClient;
 
 //import com.mongodb.client.
 
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.result.InsertOneResult;
 
 @Configuration
 @ComponentScan
 @RestController
 public class RestaurantController {
-	 int min = 01;
-	 int max = 1000;
-	private static final Log log = LogFactory.getLog(RestaurantController.class) ;
+	int min = 01;
+	int max = 1000;
+	private static final Log log = LogFactory.getLog(RestaurantController.class);
 
 	@Autowired
 	private RestaurantService restaurantService;
 
+	ConnectionString connectionString = new ConnectionString(
+			"mongodb+srv://123:90493298@cluster0.ebdqh.mongodb.net/RestaurantMicroserviceProject?retryWrites=true&w=majority");
+	MongoClientSettings settings = MongoClientSettings.builder().applyConnectionString(connectionString).build();
+	MongoClient mongoClient = MongoClients.create(settings);
+	MongoDatabase database = mongoClient.getDatabase("RestaurantMicroserviceProject");
+	MongoCollection<Document> collection = database.getCollection("RestaurantInfo");
+
 	@GetMapping("/addRest")
-	
-//	@ResponseBody hello(@RequestParam(value = "name", defaultValue = "World") String name)
 	public void AddRestaurant() {
-		ConnectionString connectionString = new ConnectionString("mongodb+srv://123:90493298@cluster0.ebdqh.mongodb.net/RestaurantMicroserviceProject?retryWrites=true&w=majority");
-		MongoClientSettings settings = MongoClientSettings.builder()
-		        .applyConnectionString(connectionString)
-		        .build();
-		MongoClient mongoClient = MongoClients.create(settings);
-		MongoDatabase database = mongoClient.getDatabase("RestaurantMicroserviceProject");
-//		database.
-		log.info(database.listCollectionNames());
-		log.info(database.getCollection("RestaurantInfo", RestaurantInfo.class));
-//		int randomNum = ThreadLocalRandom.current().nextInt(min, max + 1);
-//	    MongoClientURI uri  = new MongoClientURI("mongodb://localhost:27017/test"); 
-//        MongoClient client = new MongoClient(uri);
-//        MongoDatabase db = client.getDatabase(uri.getDatabase());
-//		MongoOperations ops = new MongoTemplate(MongoClients.create(),"RestaurantMicroserviceProject");
-//
-//				ops.insert(new RestaurantInfo("dbtest1", randomNum, "nice", randomNum));
-//		log.info(ops.findOne(new Query(where("RestName").is("dbtest1")), RestaurantInfo.class));
+		try {
+			InsertOneResult insert = collection.insertOne(new Document()
+					.append("_id",  new ObjectId())
+					.append("RestName", "99Rest")
+					.append("RestId", 99)
+					.append("RestDescription", "99RestDesc"));
+			
+			log.info("insert successful" + insert);
+			
+		} catch (MongoException e) {
+			log.error(e);
+		}
+
 	}
 
 	@GetMapping("/")
-//	@ResponseBody
-	public void index() {
-		MongoOperations ops = new MongoTemplate(MongoClients.create(),"RestaurantMicroserviceProject");
-//		MongoDatabase db = mongoClient.getDatabase("RestaurantMicroserviceProject");
-		MongoCollection<Document> restList = ops.getCollection("RestaurantInfo");
-		log.info(restList);
-		
-//		return restList.find();
-//		log.info(ops.getCollectionNames());
-//		log.info(ops.getCollection("restaurant"));
-		//return restaurantService.retrieveAllRestaurant();
+	public String index() {
+		Bson bson = Projections.fields(Projections.include("_id", "RestId", "RestName", "RestDescription"));
+		Document doc = collection.find().projection(bson).first();
+		log.info("*** " + doc.toJson());
+		return doc.toJson();
 	}
-
-
 
 	@PostMapping("/restaurant/{id}")
 //	@ResponseBody
 	public ResponseEntity<Void> registerRastaurant(@PathVariable int id, @RequestBody RestaurantInfo newRestaurant) {
 		HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/json");    
+		headers.add("Content-Type", "application/json");
 		RestaurantInfo restaurant = restaurantService.createRestaurant(id, newRestaurant);
 
 		if (restaurant == null) {
 			return ResponseEntity.noContent().build();
 		}
-		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(restaurant.getrestId())
-				.toUri();
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(restaurant.getrestId()).toUri();
 
 		return ResponseEntity.created(location).build();
 	}
